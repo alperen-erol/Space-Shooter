ac17717b298ac5834053c83b2e8817ce5495e142
Alperen Erol <alpereneroltr@gmail.com>
2024-10-10 22:19:01 +0300

    Setup

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private float _speed = 4f;
+    
+
+    void Start()
+    {
+        float randomX = Random.Range(-8f, 8f);
+        transform.position = new Vector3(Random.Range(-9f,9f), 7.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float randomX = Random.Range(-8f, 8f);
+        transform.Translate(Vector3.down*Time.deltaTime*_speed);
+        if (transform.position.y <= -5.4)
+        {
+            transform.position = new Vector3(Random.Range(-9f, 9f), 7.5f);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        
+
+        
+        if(other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        
+
+        if (other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+
+            if (player != null) {
+
+                player.Damage();
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.1f;
+    private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        calculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            fireLaser();
+        }
+    }
+    void calculateMovement()
+    {
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(horizontalInput, verticalInput) * Time.deltaTime * _speed);
+
+        if (transform.position.x > 11.3 || transform.position.x < -11.3)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y);
+        }
+    }
+    void fireLaser()
+    {
+
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, new Vector3(transform.position.x, (transform.position.y + 0.8f)), Quaternion.identity);
+    }
+    public void Damage()
+    {
+        _lives--;
+
+        if (_lives < 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
